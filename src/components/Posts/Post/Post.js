62507ffbf59ff65a581548e566ffd4dfd5b02e0c
xd00099 <dxiang@ucsd.edu>
2022-09-17 23:55:40 -0700

    basic set-up with backend + database done

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
 import useStyles from './styles';
 
 const Post = () => {
+    const posts = useSelector((state) => state.posts);
     const classes = useStyles();
+
+    console.log(posts);
+    
     return (
         <h1>POST</h1>
     );
