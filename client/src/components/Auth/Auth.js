233d142eb3e271b3286c6bd1da600e8fb728b0e8
xd00099 <dxiang@ucsd.edu>
2022-09-20 03:38:39 -0700

    google oauth2.0 done

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import useStyles from './styles';
+import Input from './input';
+import jwt_decode from 'jwt-decode';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import { useGoogleLogin, GoogleLogout, GoogleLogin } from '@react-oauth/google';
+
+
+const Auth = () => {
+    const classes = useStyles();
+    const [showPassword, setShowPassword] = useState(false);
+    const [isSignup, setIsSignup] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleSubmit = () => {
+        
+    };
+    const handleChange = () => {
+
+    };
+    const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
+    const switchMode = () => {
+        setIsSignup(!isSignup);
+        handleShowPassword(false);
+    }
+
+    const googleSuccess = async (res) => {
+        // console.log(res);
+        const token = res?.credential;
+        const result = jwt_decode(res?.credential);
+        console.log(result);
+
+        try {
+            dispatch({type: 'AUTH', data: {result, token}});
+
+            navigate('/');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant='h5'>{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {
+                            isSignup && (
+                                <>
+                                    <Input name='firstName' label='First Name' handleChange={handleChange} autoFocus half />
+                                    <Input name='lastName' label='Last Name' handleChange={handleChange} half />
+                                </>
+                            )
+                        }
+                        <Input name = 'email' label='Email Address' handleChange={handleChange} type='email'></Input>
+                        <Input name = 'password' label='Password' handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
+                        { isSignup && <Input name='confirmPassword' label='Repeart Password' handleChange={handleChange} type='password' /> }
+                    </Grid>
+                    <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>
+                        {isSignup ? 'Sign Up' : 'Sign In'}
+                    </Button>
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
+                        <GoogleLogin type='standard' theme='filled_blue' onSuccess={(response) => googleSuccess(response)}></GoogleLogin>
+                    </div>
+                    <Grid container justifyContent='flex-end'>
+                        <Grid item>
+                            <Button onClick={switchMode}>
+                                { isSignup ? "Already have an account? Sign In" : "Don't have an account? Sign Up"}
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
+    )
+}
+
+export default Auth
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -12,10 +12,11 @@ const Posts = ({ setCurrentId }) => {
     return (
         !posts.length ? <CircularProgress /> : (
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {console.log(posts)}
                 {posts.map((post) => (
-                    <Grid key={post._id} item xs={12} sm={6} md={6}>
-                        <Post post={post} setCurrentId={setCurrentId}/>
-                    </Grid>
+                <Grid key={post._id} item xs={12} sm={6} md={6}>
+                    <Post post={post} setCurrentId={setCurrentId} />
+                </Grid>
                 ))}
             </Grid>
         )
