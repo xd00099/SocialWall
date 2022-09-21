6fb77b556fb9d00c75304ff1b9bf5e66cd170b94
xd00099 <dxiang@ucsd.edu>
2022-09-20 21:23:20 -0700

    edit readme

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,19 @@
-import express from "express";
+import express from 'express';
 
-import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
+import { getPosts, getPostsBySearch, getPostsByCreator, getPost, createPost, updatePost, likePost, commentPost, deletePost } from '../controllers/posts.js';
 
 const router = express.Router();
+import auth from "../middleware/auth.js";
 
+router.get('/creator', getPostsByCreator);
+router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
-router.post('/', createPost);
-router.patch('/:id', updatePost);
-router.delete('/:id', deletePost);
-router.patch('/:id/likePost', likePost);
+router.get('/:id', getPost);
+
+router.post('/', auth,  createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
+router.post('/:id/commentPost', commentPost);
 
 export default router;
